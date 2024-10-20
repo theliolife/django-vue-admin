d12940f1f7c40c7c7e5cb14e1270e54eb30c3608
yanhui <237890221@qq.com>
2024-10-20 18:32:43 +0800

    fix

--- server/scripts/test.py
+++ server/scripts/test.py
@@ -7,22 +7,26 @@ import os
 import sys
 import logging
 from datetime import datetime
+
+from maotai.imaotai import price
 from server.settings import LOG_PATH
 import akshare as ak
 
 
 
 def run(*args):
-    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20210907',
-                                            adjust="")
-    print(stock_zh_a_hist_df)
-    exit(1)
-
-    print(sys.argv)
+    price.run()
+    
+    # stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20210907',
+    #                                         adjust="")
+    # print(stock_zh_a_hist_df)
+    # exit(1)
+    #
+    # print(sys.argv)
     # logger = logging.getLogger('log')
     # logger.info('请求成功！ response_code:{}；response_headers:{}；response_body:{}')
     # logger.error('请求出错-{}')
 
     # stat_all_index()
-    runGuess()
+    # runGuess()
     # runQuarter()
